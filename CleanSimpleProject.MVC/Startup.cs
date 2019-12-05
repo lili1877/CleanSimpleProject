a00b86be69f2b8911082499e0441eaa08d495d6c
leila <44631400+lili1877@users.noreply.github.com>
2019-12-05 13:51:14 -0800

    Remove MVC project and Create with .NetCore 3.1

--- CleanSimpleProject.MVC/Startup.cs
+++ CleanSimpleProject.MVC/Startup.cs
@@ -1,17 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CleanSimpleProject.Core.Entities.Entities;
-using CleanSimpleProject.EFCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CleanSimpleProject.MVC
 {
@@ -19,44 +15,19 @@ namespace CleanSimpleProject.MVC
     {
         public Startup(IConfiguration configuration)
         {
-            _config = configuration;
-        }
-        public Startup(IHostingEnvironment env)
-        {
-            _env = env;
-
-           // var Builder = new ConfigurationBuilder()
-           //.SetBasePath(_env.ContentRootPath)
-           //.AddJsonFile("appsettings.json")
-           //.AddEnvironmentVariables();
-           // _config = Builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfiguration _config { get; }
-         private IHostingEnvironment _env;
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-            //services.AddIdentity<CleanUser, IdentityRole>()
-             //.AddEntityFrameworkStores<CleanContext>();
-            //services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme,
-            //  opt =>
-            //  {
-            //        //configure your other properties
-            //        opt.LoginPath = "/Auth/Login";
-            //  });
-           // services.AddDbContext<CleanContext>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -68,16 +39,18 @@ namespace CleanSimpleProject.MVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
