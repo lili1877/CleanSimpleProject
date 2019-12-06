96189daba8582cd32cbb9864212008fe9de89bb9
leila <44631400+lili1877@users.noreply.github.com>
2019-12-06 00:00:23 -0800

    Comment for projectRepository

--- CleanSimpleProject.EFCore/DB.Services/ProjectRepository.cs
+++ CleanSimpleProject.EFCore/DB.Services/ProjectRepository.cs
@@ -16,6 +16,11 @@ namespace CleanSimpleProject.EFCore.DB.Services
         {
           
         }
+        /// <summary>
+        /// get all pictures for specific project
+        /// </summary>
+        /// <param name="projectId">id of specific project</param>
+        /// <returns>a list of all pictures for project with <paramref name="projectId"/></returns>
         public async Task<IEnumerable<ProjectPic>> GetAllPics(int projectId)
         {
            return await _context.ProjectPics
@@ -23,12 +28,25 @@ namespace CleanSimpleProject.EFCore.DB.Services
                           .ToListAsync();
                   
         }
+        /// <summary>
+        /// get all pictures with specific type for example 2D or 3D for specific project and 
+        /// </summary>
+        /// <param name="projectId">id of specific project</param>
+        /// <param name="type">determine the typeof pictures</param>
+        /// <returns><para>a list of all pictures with specific type for project 
+        /// with <paramref name="projectId"/>
+        /// </para></returns>
         public async Task<IEnumerable<ProjectPic>> GetAllPicsByType(int projectId, byte type)
         {
            return await _context.ProjectPics
                            .Where(o => o.ProjectId == projectId && o.Type == type)
                            .ToListAsync();
         }
+        /// <summary>
+        /// get the number of all pictures for specific project
+        /// </summary>
+        /// <param name="projectId">id of specific project</param>
+        /// <returns>an int value of the number of pictures for project with <paramref name="projectId"/></returns>
         public async Task<int> CountProjectPic(int projectId)
         {
            return await _context.ProjectPics
